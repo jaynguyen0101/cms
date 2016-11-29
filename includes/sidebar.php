6745df577edb2b32bc8c9052415fe26356c22a33
Jay Nguyen <jaynguyen@jays-mbp.attlocal.net>
2016-11-28 21:22:30 -0800

    reusable code for index admin and index page

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,13 +1,5 @@
 <div class="col-md-4">
 
-    <?php
-
-        if(isset($_POST['submit'])) {
-            $search = $_POST['search'];
-        }
-
-    ?>
-
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
@@ -15,7 +7,7 @@
         <div class="input-group">
             <input name="search" type="text" class="form-control">
             <span class="input-group-btn">
-                <button class="btn btn-default" type="submit">
+                <button name="submit" class="btn btn-default" type="submit">
                     <span class="glyphicon glyphicon-search"></span>
             </button>
             </span>
@@ -27,41 +19,26 @@
     <!-- Blog Categories Well -->
     <div class="well">
         <h4>Blog Categories</h4>
+        <?php 
+            $query = "SELECT  * FROM categories";
+            $select_categories_sidebar = mysqli_query($connection, $query);
+        ?>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                    <?php 
+                        while($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                            $cat_title = $row['cat_title'];
+                            echo "<li><a href='#'>{$cat_title}</a></li>";
+                        }
+                    ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include "widget.php" ?>
 
 </div>
